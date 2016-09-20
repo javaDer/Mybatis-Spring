393ff2fe145af1899c27385f10227b22abf771c1
javaDer <fa20091001@163.com>
2016-09-20 15:47:40 +0800

    添加user信息放置在redis中

--- src/main/java/com/cms/model/UserInfo.java
+++ src/main/java/com/cms/model/UserInfo.java
@@ -1,27 +1,3 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2014 abel533@gmail.com
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package com.cms.model;
 
 import javax.persistence.*;
@@ -58,8 +34,6 @@ public class UserInfo {
      */
     private String realname;
 
-    private String openid;
-
     /**
      * QQ
      */
@@ -71,6 +45,8 @@ public class UserInfo {
      * 联系电话
      */
     private String tel;
+
+    private String openid;
 
     /**
      * @return Id
@@ -226,12 +202,17 @@ public class UserInfo {
         this.tel = tel;
     }
 
+    /**
+     * @return openid
+     */
     public String getOpenid() {
         return openid;
     }
 
+    /**
+     * @param openid
+     */
     public void setOpenid(String openid) {
         this.openid = openid;
     }
-
 }
