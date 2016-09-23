27e55ce9f2e6a0db9c2add6ff4d3a7528b8f2f2e
javaDer <fa20091001@163.com>
2016-09-24 02:36:26 +0800

    创建旅游项目模型

--- src/main/java/com/cms/controller/TravelController.java
+++ src/main/java/com/cms/controller/TravelController.java
@@ -1,20 +1,1 @@
-package com.cms.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-/**
- * Created by jack on 16-9-24.
- */
-@Controller
-@RequestMapping(value = "/travel")
-public class TravelController {
-    @RequestMapping(value="/index")
-    public ModelAndView toindex(){
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("travel/index");
-        return mv;
-    }
-
-}
+package com.cms.controller;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.servlet.ModelAndView;/** * Created by jack on 16-9-24. */@Controller@RequestMapping(value = "/travel")public class TravelController {    @RequestMapping(value="/index")    public ModelAndView toindex(){        ModelAndView mv = new ModelAndView();        mv.setViewName("travel/index");        return mv;    }}
